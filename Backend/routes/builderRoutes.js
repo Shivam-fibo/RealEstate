e54bbca62e383f598076dc8f79f0063725125414
Shivam <sg641818@gmail.com>
2025-07-15 09:07:27 +0530

    Re-initialize with frontend-only code

--- Backend/routes/builderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { createBuilder } from '../controllers/builderController.js'
-
-const router = express.Router()
-
-router.post('/', createBuilder)
-
-export default router
